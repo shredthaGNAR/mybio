31dfab6cee9002a89c1279102ddf47ba6522c736
Me <37919512+shredthaGNAR@users.noreply.github.com>
2026-04-06 06:14:19 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ export default defineConfig({
   // User/Org Pages (https://<user>.github.io):
   //   site: 'https://<user>.github.io',
   //   base: '/'
-//Project Pages (https://<user>.github.io/<repo>/):
-    site: 'https://<user>.github.io/<repo>/',
-    base: '/<repo>/'
-  // site: 'https://your-user.github.io',
- //  base: '/',
+  // Project Pages (https://<user>.github.io/<repo>/):
+     site: 'https://<user>.github.io/<repo>/',
+     base: '/<repo>/'
+ // site: 'https://your-user.github.io',
+ // base: '/',
 });
